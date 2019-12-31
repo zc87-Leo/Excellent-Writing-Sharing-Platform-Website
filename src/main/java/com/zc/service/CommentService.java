e387a18e39e1ad1c3d50300a7a0eced88195c5ab
zc87-Leo <zhangcheng960807@163.com>
2019-12-31 17:03:55 +0800

    1. complete comments part ( but not post successfully)

---
+++ src/main/java/com/zc/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.zc.service;
+
+import com.zc.po.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+
+}
